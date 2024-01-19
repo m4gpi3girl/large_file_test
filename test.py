1014ddaae7086983ac4f4595683c6d23da279067
EL <m4gpi3girl@gmail.com>
2024-01-19 12:03:21 +0000

    unicode escape error fix

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ def main():
 
     st.title("Test for GIT LFS")
 
-    df = pd.read_csv("C:\Users\Elisha.Zissman\OneDrive - Social Investment Business\Desktop\GIT_LFS_TEST\LARGEDATA\pc_data.csv")
+    df = pd.read_csv(r"C:\Users\Elisha.Zissman\OneDrive - Social Investment Business\Desktop\GIT_LFS_TEST\LARGEDATA\pc_data.csv")
     st.write(df.head())
 
 if __name__ == "__main__":
