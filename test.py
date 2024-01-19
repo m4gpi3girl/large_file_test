199247abeb8b859a00744701688e7cc135dca0a5
EL <m4gpi3girl@gmail.com>
2024-01-19 11:35:41 +0000

    first commit

---
+++ test.py
@@ -0,0 +1,13 @@
+import pandas as pd 
+import streamlit as st 
+
+def main():
+
+    st.title("Test for GIT LFS")
+
+    df = pd.read_csv("C:\Users\Elisha.Zissman\OneDrive - Social Investment Business\Desktop\GIT_LFS_TEST\LARGEDATA\pc_data.csv")
+    st.write(df.head())
+
+if __name__ == "__main__":
+    main()
+
